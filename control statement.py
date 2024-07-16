f6f4c6f30de53435e16be87d11d1b075575b85dd
Nithyasadha <nithyasumathi27@gmail.com>
2024-07-16 16:41:23 -0700

    file1 is created

---
+++ control statement.py
@@ -0,0 +1,31 @@
+#Control statements
+
+#BREAK
+
+#Program to break when the number is 5
+'''i=1
+while i<10:
+    if i==5:
+        break
+    else:
+        print(i)
+    i+=1
+
+#CONTINUE
+
+#Program to skip all the even numbers
+t_=(34,5,3,68,4,56,34,907)
+for num in t_:
+    if num%2==0:
+        continue
+    print(num)'''
+
+#PASS
+
+#'Pass keyword is
+
+Use it for later use'
+for i in [4,5,6]:
+    pass
+for i in 'hai':
+    print(i)
